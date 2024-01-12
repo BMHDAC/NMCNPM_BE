3498115d8f757e410914253aee2055a432e9463b
BuiDac <dac.bui308@gmail.com>
2024-01-12 12:11:48 +0700

    Delete backend

--- routes/refresh.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const refreshTokenController = require('../controllers/refreshTokenController');
-const verifyJWT = require('../middleware/verifyJWT');
-
-router.use(verifyJWT)
-router.get('/', refreshTokenController.handleRefreshToken);
-
-module.exports = router;
